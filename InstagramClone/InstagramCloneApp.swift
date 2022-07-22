d3175272d8667112db0c8ecf860202896abca175
YossiBenZaken <yossibz@gmail.com>
2022-07-22 17:46:53 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Yosef Ben Zaken on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
